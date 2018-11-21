4253e0bb77228c01034a0f587f4f83db6453ff43
Chris Klanac <cklanac@users.noreply.github.com>
2018-11-21 18:54:25 -0500

    fix: location path

--- routes/notes.router.js
+++ routes/notes.router.js
@@ -80,13 +80,16 @@ router.post('/notes', (req, res, next) => {
     return next(err);
   }
 
-  notes.create(newItem)
-    .then(item => {
-      if (item) {
-        res.location(`http://${req.headers.host}/notes/${item.id}`).status(201).json(item);
-      }
-    })
-    .catch(err => next(err));
+  notes.create(newItem, (err, item) => {
+    if (err) {
+      return next(err);
+    }
+    if (item) {
+      res.location(`http://${req.headers.host}/notes/${item.id}`).status(201).json(item);
+    } else {
+      next();
+    }
+  });
 });
 
 // Delete an item
